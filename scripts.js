795de0f66ddf293afb8ef584fb776c29ba5fa73d
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-08-27 12:22:25 +0200

    tot lo de  can sici
    es per crerar elements

--- scripts.js
+++ scripts.js
@@ -1,4 +1,32 @@
-var newelement = document.createElement('p')
-newelement.scr = "holaa"
+var elbuto = document.getElementById('mesbtns');
 
-document.body.appendChild(newelement)
+elbuto.addEventListener('click', function() {
+  var nouele = document.createElement(tipuselement);
+  var tipuselement = document.getElementById('tipusele').value;
+  var contele = document.getElementById('contele').value;
+  var stylech = document.getElementById('stylech');
+  var select = document.getElementById('selection');
+
+  nouele.textContent = contele;
+  if (stylech.checked) {
+    console.log(contele);
+    nouele.style.color = 'blue';
+  }
+ else {
+   nouele.style.color = 'none';
+   console.log(nouele);
+ }
+   var totesOpcions = select.selectedIndex;
+   var opSelect = select.options[totesOpcions].value;
+  
+  if (opSelect === 'color') {
+    nouele.style.color = 'purple';
+  }
+  else if (opSelect === "size") {
+    nouele.style.size = '28';
+  }
+  else {
+    console.log('error')
+  }
+  document.body.appendChild(nouele);
+});
