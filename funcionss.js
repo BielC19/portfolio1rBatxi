5c42136a0463e912e3f8728423d220df3ddfab04
BielC19 <118078513+BielC19@users.noreply.github.com>
2023-08-28 23:34:00 +0200

    new file:   funcionss.js 	modified:   index.css 	modified:   index.html 	modified:   scripts.js     modified:   funcionss.js

---
+++ funcionss.js
@@ -0,0 +1,8 @@
+export function resselect() {
+    return alert("Has de seleccionar algo siusplau.")
+}
+export function boto() {
+    var contingut = document.createElement('input')
+    
+
+}
--- scripts.js
+++ scripts.js
@@ -1,8 +1,31 @@
+import {resselect, boto}from './funcionss'
+var seleccionant = document.getElementById('selecciotipius');
+seleccionant.addEventListener('select', function() {
+  var infoseleccio = seleccionant.options[seleccionant.selectedIndex].value;
+  if (infoseleccio === '') {
+    resselect();
+  }
+  else if (infoseleccio === 'boto') {
+    boto();
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+/*
 var elbuto = document.getElementById('mesbtns');
 
 elbuto.addEventListener('click', function() {
-  var nouele = document.createElement(tipuselement);
   var tipuselement = document.getElementById('tipusele').value;
+  var nouele = document.createElement(tipuselement);
   var contele = document.getElementById('contele').value;
   var stylech = document.getElementById('stylech');
   var select = document.getElementById('selection');
@@ -12,21 +35,24 @@ elbuto.addEventListener('click', function() {
     console.log(contele);
     nouele.style.color = 'blue';
   }
- else {
-   nouele.style.color = 'none';
-   console.log(nouele);
- }
-   var totesOpcions = select.selectedIndex;
-   var opSelect = select.options[totesOpcions].value;
+  else {
+    nouele.style.color = 'none';
+    console.log(nouele);
+  }
+    var totesOpcions = select.selectedIndex;
+    var opSelect = select.options[totesOpcions].value;
   
   if (opSelect === 'color') {
     nouele.style.color = 'purple';
   }
   else if (opSelect === "size") {
-    nouele.style.size = '28';
+    nouele.style.fontSize = '40px';
+    nouele.style.left = '50px'
   }
   else {
     console.log('error')
+    alert("Error")
   }
   document.body.appendChild(nouele);
 });
+*/
